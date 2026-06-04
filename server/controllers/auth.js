e8721d475f3934be6cb9618c1275cfe8fd9df4d8
Saikat Pradhan <saikatpradhan65@gmail.com>
2026-06-04 20:19:58 +0530

    Server Added

---
+++ server/controllers/auth.js
@@ -0,0 +1,37 @@
+import User from "../models/User.js";
+import generateToken from "../config/token.js";
+
+export const googleAuth = async (req, res) => {
+    try {
+        const { name, email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ name, email });
+        }
+
+        let token = generateToken(user._id);
+        res.cookie("token", token, {
+            http: true,
+            secure: false, 
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.status(200).json({ message: "Google Auth successful", user });
+    } catch (error) {
+        console.error("Error in Google Auth:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        await res.clearCookie("token");
+        return res.status(200).json({ message: "Logout successful" });
+    } catch (error) {
+        console.error("Error in Logout:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import connectDB from './config/connectDB.js';
+import authRouter from './routes/auth.js';
+import userRouter from './routes/user.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookiesParser from 'cookie-parser';
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookiesParser());
+
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
+});
