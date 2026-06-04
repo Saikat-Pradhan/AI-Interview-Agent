e640ffbd043150af13c0987342c8f4337bc7bbac
Saikat Pradhan <saikatpradhan65@gmail.com>
2026-06-04 21:00:10 +0530

    v

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { googleAuth, logout } from '../controllers/auth.js';
-
-const authRouter = express.Router();
-
-authRouter.post('/google', googleAuth);
-authRouter.post('/logout', logout);
-
-export default authRouter;
