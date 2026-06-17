ddc7e79411dd70ef9b2265bcf0d2ce2826951482
Saikat Pradhan <saikatpradhan65@gmail.com>
2026-06-17 21:26:19 +0530

    Fix deleteItem function to correctly pass options for axios request

--- client/src/pages/InterviewHistory.jsx
+++ client/src/pages/InterviewHistory.jsx
@@ -25,7 +25,7 @@ const InterviewHistory = () => {
   const deleteItem = async (id) => {
     try {
       setOpenDeleteId(null)
-      await axios.post(serverURL + `/api/interview/delete/${id}`, { withCredentials: true })
+      await axios.post(serverURL + "/api/interview/delete/" + id, {}, { withCredentials: true })
       setTimeout(() => {
         navigate("/history");
       }, 3000);
